bf049229e78899e7f0a0174b00da7e944ad33149
ytsr1988 <dragonpw1988@gmail.com>
2016-10-26 00:30:49 -0600

    Add code to test how constructor works

--- JavaClassTest/src/ConstructorsTest.java
+++ JavaClassTest/src/ConstructorsTest.java
@@ -1,4 +1,34 @@
+public class ConstructorsTest {
+	public static void main(String[] args){
+		foo b = new foo("harry");
+		foo c = new foo();
+		foo d=b;  // d and b are references to the same object
+		// There are only two foo objects in this code.
+		b.showName();
+		c.showName();
+		d.showName();
+		System.out.println("--------");
+		b.changeName("Jack");
+		b.showName();
+		d.showName();
+	}
 
-public class A1 {
+}
 
+class foo {
+	private String Name = "No name";
+	void showName(){
+		System.out.println("The name is "+Name);
+	}
+	void changeName(String x){
+		System.out.println("name changed from "+ Name + " to "+ x);
+		Name = x;
+	}
+	foo(){
+		System.out.println("Construct object from NULL");
+	}
+	foo(String x){
+		Name = x;
+		System.out.println("Construct object from string: "+x);
+	}
 }
