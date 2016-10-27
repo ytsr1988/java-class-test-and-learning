9b81a8e47698a25aa6a98fa5adc6522b34155a91
DragonPW <wangyao.pw@gmail.com>
2016-10-27 17:16:14 -0600

    Add new test file

---
+++ JavaClassTest/src/fundermentals/JavaBasics.java
@@ -0,0 +1,10 @@
+package fundermentals;
+
+public class JavaBasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
